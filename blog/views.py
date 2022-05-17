41a2adef31d9ed3c34f4d2ba0ebc70a047091bf8
hoo0 <hoo.matrix@gmail.com>
2022-05-17 23:21:10 +0000

    Added login view.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 from .models import Post
 from .forms import PostForm
@@ -14,6 +15,7 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+@login_required
 def post_new(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
@@ -22,7 +24,7 @@ def post_new(request):
             post = form.save(commit=False) #바로 저장하지 않고 데이터셋팅만한다
             #추가셋팅
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save() #저장
             return redirect('post_detail', pk=post.pk)
     else:
@@ -30,6 +32,7 @@ def post_new(request):
         
     return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     
@@ -40,10 +43,30 @@ def post_edit(request, pk):
             post = form.save(commit=False)
             
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post) #GET, instance:내용셋팅
         
     return render(request, 'blog/post_edit.html', {'form': form})
+
+# def login(request):
+#     return render(request, 'registration/login.html')
+
+@login_required
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
